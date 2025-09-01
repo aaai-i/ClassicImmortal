c1ac273c376f879639ab086a4cef06a79e3de460
彭彭 <appa21@qq.com>
2025-09-01 10:46:13 +0800

    后端框架

---
+++ classic-immortal-server/src/main/java/com/example/classicimmortalserver/ClassicImmortalServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.classicimmortalserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassicImmortalServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassicImmortalServerApplication.class, args);
+	}
+
+}
---
+++ classic-immortal-server/src/test/java/com/example/classicimmortalserver/ClassicImmortalServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.classicimmortalserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassicImmortalServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
