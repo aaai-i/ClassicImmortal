069013595143e8548aff50f1d267a859b1d664a4
彭彭 <appa21@qq.com>
2025-09-03 09:08:43 +0800

    合并远程更改

---
+++ vue/src/views/Login.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="login-one">
+        <div class="login-two">
+
+        </div>
+    </div>
+</template>
+
+
+<script setup>
+
+
+</script>
+
+
+<style scoped>
+
+
+
+.login-one{
+    height: 100vh;
+    background-image: url("@/assets/loginone.png");
+}
+
+
+</style>
